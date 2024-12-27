76d8f171608e4ab3d2b3e1fba8d6d66f41ccc69e
Suji M <Manthri.Sujitha@coforge.com>
2024-12-27 10:00:19 +0530

    completed downloading reports

--- src/app/components/user-score/user-score.component.ts
+++ src/app/components/user-score/user-score.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { saveAs } from 'file-saver';
+import { AuthenticationService } from '../../services/authentication.service';
 
 
 @Component({
@@ -9,11 +10,15 @@ import { saveAs } from 'file-saver';
   styleUrls: ['./user-score.component.css']
 })
 export class UserScoreComponent {
-  userId: number = 4; // Static user ID for testing, replace with dynamic value as needed
+  userId: number = 0; // Static user ID for testing, replace with dynamic value as needed
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private authService: AuthenticationService) {}
 
   downloadPdf(): void {
+
+    this.userId = this.authService.id;
+    
     const url = `http://localhost:7777/api/user/score/${this.userId}/download`;
     this.http.get(url, { responseType: 'blob' }).subscribe((response: Blob) => {
       const fileName = 'UserScore.pdf';
